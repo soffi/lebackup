b8e3e8806f1be0d787963f0ed4bb303b2dd2f1ab
soffi <soffanias@gmail.com>
2016-03-21 14:46:28 -0400

    updates

--- lebackup-vm.sh
+++ lebackup-vm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+source $(dirname "$0")/config-vars
 VM=$1
 if [ -z $1 ];
 	then
@@ -9,16 +11,16 @@ BLOCKDEVTYPE=$(virsh domblklist $VM|tail -2|head -1|awk '{print $1}')
 BLOCKDEV=$(virsh domblklist $VM|tail -2|head -1|awk '{print $2}')
 
 echo creating snapshot
-virsh snapshot-create-as --domain $VM $VM-snap --diskspec $BLOCKDEVTYPE,file=/scratch/$VM-snap.qcow2 --disk-only --atomic
+virsh snapshot-create-as --domain $VM $VM-snap --diskspec $BLOCKDEVTYPE,file=$SCRATCHDIR/$VM-snap.qcow2 --disk-only --atomic
 
 echo backing up
-rsync -avh --progress $BLOCKDEV /backup/$VM-backup-$(date +"%m-%d-%Y-%H%M").qcow2
+rsync -avh --progress $BLOCKDEV $BACKUPDIR/$VM-backup-$(date +"%m-%d-%Y-%H%M").qcow2
 
 echo merging snapshot
 virsh blockcommit $VM $BLOCKDEVTYPE --active --verbose --pivot
 
 echo deleting snapshot
 virsh snapshot-delete $VM $VM-snap --metadata
-rm /scratch/$VM-snap.qcow2
+rm $SCRATCHDIR/$VM-snap.qcow2
 
 echo finished!
