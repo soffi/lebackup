e4f10f347e51a2c4b1a234599ecdf97cca4afdfc
soffi <soffanias@gmail.com>
2016-03-22 22:31:43 -0400

    dont mount nfs, keep it mounted by user

--- lebackup-all.sh
+++ lebackup-all.sh
@@ -2,15 +2,15 @@
 set -e
 source $(dirname "$0")/config-vars
 # mount and create backup target 
-mkdir -p $BACKUPDIR
-mount $NFSSERVER $BACKUPDIR
+#mkdir -p $BACKUPDIR
+#mount $NFSSERVER $BACKUPDIR
 #back up files
 $(dirname "$0")/lebackup-files.sh
 #back up vms
 if [ -z "$VMS" ];
 then
 	echo no vms
-	umount $BACKUPDIR
+	#umount $BACKUPDIR
 	rm -Rf $BACKUPDIR
 	exit 0
 fi
@@ -19,5 +19,5 @@ for VM in $VMS
 		$(dirname "$0")/lebackup-vm.sh $VM
 	done
 # remove and unmout backupdir
-umount $BACKUPDIR
-rm -Rf $BACKUPDIR
+#umount $BACKUPDIR
+#rm -Rf $BACKUPDIR
