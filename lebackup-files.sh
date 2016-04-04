52ae47a0bed146b5dad9a078df13e94b0cce1c05
soffi <soffanias@gmail.com>
2016-04-04 13:34:58 +0000

    dont compress

--- lebackup-files.sh
+++ lebackup-files.sh
@@ -2,6 +2,6 @@
 source $(dirname "$0")/config-vars
 STARTTIME=$(date +"%m-%d-%Y-%H%M")
 mkdir -p $SCRATCHDIR
-tar cvjf $SCRATCHDIR/$(hostname)-filebackup-$STARTTIME.tar.bz2 $(cat $(dirname "$0")/backup-files.list)
-rsync -avh --no-o --no-g --progress $SCRATCHDIR/$(hostname)-filebackup-$STARTTIME.tar.bz2 $RSYNCSERVER
-rm $SCRATCHDIR/$(hostname)-filebackup-$STARTTIME.tar.bz2
+tar cvf $SCRATCHDIR/$(hostname)-filebackup-$STARTTIME.tar $(cat $(dirname "$0")/backup-files.list)
+rsync -avh --no-o --no-g --progress $SCRATCHDIR/$(hostname)-filebackup-$STARTTIME.tar $RSYNCSERVER
+rm $SCRATCHDIR/$(hostname)-filebackup-$STARTTIME.tar
